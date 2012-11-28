1876dc7e871c2d7af0cacc42c031b2d49327f524
Will Jones <will@sacharissa.co.uk>
2012-11-28 13:23:25 +0000

    Add a smart character-by-character mode.

--- src/Language/Haskell/TinySig/Expand.hs
+++ src/Language/Haskell/TinySig/Expand.hs
@@ -15,20 +15,20 @@ simpleType :: String -> Type
 simpleType
   = TyCon . UnQual . Ident
 
-typeVariable :: String -> Type
+typeVariable :: Char -> Type
 typeVariable
-  = TyVar . Ident
+  = TyVar . Ident . (:[])
 
 expand :: [Rule] -> TinySig -> Type
 expand rs (IdentS ident)
-  | Just ty <- lookup ident rs
-      = ty
+  | Just ty <- lookup ident rs = ty
 
-  | c : cs <- ident, isUpper c
-      = simpleType ident
-
-  | otherwise
-      = typeVariable ident
+expand rs (IdentS ident@(c : cs))
+  | isUpper c = simpleType ident
+  | null cs   = ty
+  | otherwise = TyApp ty (expand rs (IdentS cs))
+  where
+    ty = typeVariable c
 
 expand rs (FunS ts1 ts2)
   = TyFun (expand rs ts1) (expand rs ts2)
@@ -40,10 +40,7 @@ expand rs (ProductS tss)
   = TyTuple Boxed (map (expand rs) tss)
 
 expand rs (SumS tss)
-  = foldl1 (TyApp . TyApp either) (map (expand rs) tss)
-    where
-      either
-        = simpleType "Either"
+  = foldl1 (TyApp . TyApp (simpleType "Either")) (map (expand rs) tss)
 
 expand rs (AppS ts1 ts2)
   = TyApp (expand rs ts1) (expand rs ts2)
